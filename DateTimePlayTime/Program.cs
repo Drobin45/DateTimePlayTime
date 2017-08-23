1919b9a66cf902a99bc6fd41c7a306e3ed109eb0
drobin45 <drobin45@kent.edu>
2017-08-23 15:15:44 -0400

    Added switch cases

--- DateTimePlayTime/Program.cs
+++ DateTimePlayTime/Program.cs
@@ -11,23 +11,106 @@ namespace DateTimePlayTime
         static void Main(string[] args)
         {
 
-            Console.WriteLine(DateTime.Today);
-           // Console.WriteLine("Today: {0}", DateTime.Today);
+            Console.WriteLine(DateTime.Today);              
+            DateTime CurrentDay = DateTime.Now;
+            int day1 = (int)CurrentDay.DayOfWeek;          
+
+            string dayOne = " ";            
+           switch (day1)
+            {
+                case 1:
+                    dayOne = "Today is Monday";
+                break;
+                case 2:
+                    dayOne = "Today is Tuesday";
+                    break;
+                case 3:
+                    dayOne = "Today is Wednesday";
+                    break;
+                case 4:
+                    dayOne = "Today is Thursday";
+                    break;
+                case 5:
+                    dayOne = "Today is Friday";
+                    break;
+                case 6:
+                    dayOne = "Today is Saturday";
+                    break;
+                case 7:
+                    dayOne = "Today is Sunday";
+                    break;
+            }
+            Console.WriteLine(dayOne);
+
+
+            DateTime tomorrow = GetTomorrow();          
+            int tom1 = (int)tomorrow.DayOfWeek;
+            
+            string tomOne = " ";
+            switch (tom1)
+            {
+                case 1:
+                    tomOne = "Tomorrow is Monday";
+                    break;
+                case 2:
+                    tomOne = "Tomorrow is Tuesday";
+                    break;
+                case 3:
+                    tomOne = "Tomorrow is Wednesday";
+                    break;
+                case 4:
+                    tomOne = "Tomorrow is Thursday";
+                    break;
+                case 5:
+                    tomOne = "Tomorrow is Friday";
+                    break;
+                case 6:
+                    tomOne = "Tomorrow is Saturday";
+                    break;
+                case 7:
+                    tomOne = "Tomorrow is Sunday";
+                    break;
+            }
+            Console.WriteLine(tomOne);
 
             DateTime yesterday = GetYesterday();
-           //Console.WriteLine("Yesterday: {0}", yesterday);
-            
-            DateTime tomorrow = GetTomorrow();
-            //Console.WriteLine("Tomorrow: {0}", tomorrow);
 
-            DateTime CurrentDay = DateTime.Now;
-            int day1 = (int)CurrentDay.DayOfWeek;
+            //translate to tomorrow
 
-           //switch(DayOfWeek) Look up this method(?)!
+            int tom1 = (int)tomorrow.DayOfWeek;
 
-           // Console.WriteLine("Today is " +);
-           //Console.WriteLine("Tomorrow is " +);
-           //Console.WriteLine("Yesterday was " +);
+            string tomOne = " ";
+            switch (tom1)
+            {
+                case 1:
+                    tomOne = "Tomorrow is Monday";
+                    break;
+                case 2:
+                    tomOne = "Tomorrow is Tuesday";
+                    break;
+                case 3:
+                    tomOne = "Tomorrow is Wednesday";
+                    break;
+                case 4:
+                    tomOne = "Tomorrow is Thursday";
+                    break;
+                case 5:
+                    tomOne = "Tomorrow is Friday";
+                    break;
+                case 6:
+                    tomOne = "Tomorrow is Saturday";
+                    break;
+                case 7:
+                    tomOne = "Tomorrow is Sunday";
+                    break;
+            }
+            Console.WriteLine(tomOne);
+
+            //switch(DayOfWeek) Look up this method(?)!
+
+            // Console.WriteLine("Today is " +);
+            //Console.WriteLine("Tomorrow is " +);
+            //Console.WriteLine("Yesterday was " +);
 
 
             //Methods Section below------------------------------------------------
