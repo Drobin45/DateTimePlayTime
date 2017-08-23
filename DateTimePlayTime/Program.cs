cba894213d7d9a8d854208bccb6b80aee7850b1c
drobin45 <drobin45@kent.edu>
2017-08-23 14:54:58 -0400

    Added two methods and 4 new variables

--- DateTimePlayTime/Program.cs
+++ DateTimePlayTime/Program.cs
@@ -10,6 +10,36 @@ namespace DateTimePlayTime
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(DateTime.Today);
+           // Console.WriteLine("Today: {0}", DateTime.Today);
+
+            DateTime yesterday = GetYesterday();
+           //Console.WriteLine("Yesterday: {0}", yesterday);
+            
+            DateTime tomorrow = GetTomorrow();
+            //Console.WriteLine("Tomorrow: {0}", tomorrow);
+
+            DateTime CurrentDay = DateTime.Now;
+            int day1 = (int)CurrentDay.DayOfWeek;
+
+           //switch(DayOfWeek) Look up this method(?)!
+
+           // Console.WriteLine("Today is " +);
+           //Console.WriteLine("Tomorrow is " +);
+           //Console.WriteLine("Yesterday was " +);
+
+
+            //Methods Section below------------------------------------------------
+        }
+        static DateTime GetYesterday()
+        {
+            return DateTime.Today.AddDays(-1);
+        }
+
+        static DateTime GetTomorrow()
+        {
+            return DateTime.Today.AddDays(1);
         }
     }
 }
